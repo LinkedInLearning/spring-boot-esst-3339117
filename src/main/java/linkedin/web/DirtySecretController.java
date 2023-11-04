3cf596ceed7e9c594ab1bf69d1df8bdaff81fc19
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-04 19:32:18 +0100

    Fixed imports

--- src/main/java/linkedin/web/DirtySecretController.java
+++ src/main/java/linkedin/web/DirtySecretController.java
@@ -2,18 +2,14 @@ package linkedin.web;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.websocket.server.PathParam;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.Collection;
 import java.util.HashMap;
