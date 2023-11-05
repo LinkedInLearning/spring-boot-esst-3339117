ea36d9748a5b4360546226199c52f15a6e63b945
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-05 19:42:09 +0100

    cleanup

--- src/main/java/linkedin/data/DirtySecretsRestController.java
+++ src/main/java/linkedin/data/DirtySecretsRestController.java
@@ -3,18 +3,13 @@ package linkedin.data;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.UUID;
-import java.util.Collection;
 
 @RestController
 @RequestMapping("/dirty-secrets")
@@ -25,8 +20,6 @@ public class DirtySecretsRestController {
   public DirtySecretsRestController(DirtySecretsRepository repository) {
     this.repository = repository;
   }
-
-  private Map<String, DirtySecret> secrets = new TreeMap<String, DirtySecret>();
 
   @GetMapping
   public Iterable<DirtySecret> get() {
