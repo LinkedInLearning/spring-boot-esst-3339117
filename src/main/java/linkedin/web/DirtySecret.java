84f8ad69fdd3dfb36f3a956596baccfbc2d4bcf2
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-20 10:04:41 +0100

    +Dirty Secret

---
+++ src/main/java/linkedin/web/DirtySecret.java
@@ -0,0 +1,34 @@
+package linkedin.web;
+
+public class DirtySecret {
+
+  private String id;
+
+  private String name;
+
+  private String secret;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSecret() {
+    return secret;
+  }
+
+  public void setSecret(String secret) {
+    this.secret = secret;
+  }
+}
