5e5c97086ae2f588a12c2958c6d1fcac35a4be0d
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-20 23:21:23 +0100

    Update test.

---
+++ src/test/java/linkedin/data/DirtySecretsRepositoryTests.java
@@ -0,0 +1,40 @@
+package linkedin.data;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+@SpringBootTest
+class DirtySecretsRepositoryTests {
+
+  @Autowired
+  private DirtySecretsRepository repository;
+
+  @Container
+  @ServiceConnection
+  static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(
+      DockerImageName.parse("postgres:latest"));
+
+  @Test
+  void shouldSaveDirtySecretInPostgres() {
+    // 1. Secret erstellen
+    var dirtySecret = new DirtySecret();
+    dirtySecret.setName("Freddy");
+    dirtySecret.setSecret("Killed an elderly couple and did time.");
+
+    // 2. Secret speichern
+    var savedSecret = this.repository.save(dirtySecret);
+
+    // 3. Generierte Id prüfen
+    assertNotNull(savedSecret.getId());
+  }
+
+}
