fe5f65064c582a428429d38b5ac7b6270d15bdae
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-20 20:21:31 +0100

    Fix starter

---
+++ src/test/java/linkedin/web/DirtySecretsRestControllerTests.java
@@ -0,0 +1,17 @@
+package linkedin.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+public class DirtySecretsRestControllerTests {
+
+  @Test
+  public void shouldSaveSecrets() throws Exception {
+  }
+
+}
