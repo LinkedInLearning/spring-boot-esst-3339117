351bbe42ef0aa92925a99ddaff282aba0a8b7491
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-20 21:56:48 +0100

    End

--- src/main/java/linkedin/web/DirtySecretsRestController.java
+++ src/main/java/linkedin/web/DirtySecretsRestController.java
@@ -1,10 +1,16 @@
 package linkedin.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+@RestController
+@RequestMapping("/dirty-secrets")
 public class DirtySecretsRestController {
 
   private DirtySecretsRepository repository;
@@ -14,8 +20,27 @@ public class DirtySecretsRestController {
   }
 
   @GetMapping("/e1/{id}")
-  public DirtySecret getById(@PathVariable String id) {
-    return this.repository.getById(id).orElseThrow();
+  public DirtySecret getByIdE1(@PathVariable String id) {
+    return this.repository.getById(id)
+        .orElseThrow(() -> new ResponseStatusException(
+            HttpStatus.NOT_FOUND,
+            ":-( Found nothing."));
+  }
+
+  @GetMapping("/e2/{id}")
+  public DirtySecret getByIdE2(@PathVariable String id) {
+    return this.repository.getById(id)
+        .orElseThrow(() -> new NoSecretFoundWebException());
+  }
+
+  @GetMapping("/e3/{id}")
+  public DirtySecret getByIdE3(@PathVariable String id) {
+    return this.repository.getById(id).orElseThrow(() -> new NoSecretFoundException());
+  }
+
+  @ExceptionHandler({ NoSecretFoundException.class })
+  public ResponseEntity<String> handleNoSecretFoundException() {
+    return ResponseEntity.internalServerError().body("No secret found.");
   }
 
 }
