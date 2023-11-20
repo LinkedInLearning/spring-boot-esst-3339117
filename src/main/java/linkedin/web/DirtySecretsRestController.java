5f159a9eb3102a69762881f4b43f202a4f6e3519
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-20 12:15:24 +0100

    Fixed end state

--- src/main/java/linkedin/web/DirtySecretsRestController.java
+++ src/main/java/linkedin/web/DirtySecretsRestController.java
@@ -1,6 +1,8 @@
 package linkedin.web;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +16,18 @@ public class DirtySecretsRestController {
     this.repository = repository;
   }
 
-  @GetMapping
+  @GetMapping("/count")
   public int count() {
     return this.repository.count();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<DirtySecret> getById(@PathVariable String id) {
+    var secret = this.repository.getById(id);
+    if (secret.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok().body(secret.get());
   }
 
 }
