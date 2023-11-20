1e6e545bef283727c19d1e66614d9027664ccbeb
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-20 12:07:23 +0100

    End state

---
+++ src/main/java/linkedin/web/DirtySecretsRestController.java
@@ -0,0 +1,22 @@
+package linkedin.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dirty-secrets")
+public class DirtySecretsRestController {
+
+  private DirtySecretsRepository repository;
+
+  public DirtySecretsRestController(DirtySecretsRepository repository) {
+    this.repository = repository;
+  }
+
+  @GetMapping
+  public int count() {
+    return this.repository.count();
+  }
+
+}
