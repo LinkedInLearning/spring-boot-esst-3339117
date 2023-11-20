4adfb3db0ba913a366273959034b6c9b8c1883ac
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-20 22:10:49 +0100

    Prep

--- src/main/java/linkedin/web/DirtySecretsRestController.java
+++ src/main/java/linkedin/web/DirtySecretsRestController.java
@@ -2,7 +2,11 @@ package linkedin.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/dirty-secrets")
 public class DirtySecretsRestController {
 
   private DirtySecretsRepository repository;
