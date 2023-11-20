8879f7a42f4a05abf26828b5a436eea4c6420d78
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-20 17:55:08 +0100

    Cleanup

--- src/main/java/linkedin/web/DirtySecretsRestController.java
+++ src/main/java/linkedin/web/DirtySecretsRestController.java
@@ -1,6 +1,5 @@
 package linkedin.web;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
