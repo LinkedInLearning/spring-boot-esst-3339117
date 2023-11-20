80b81007f52610771be0de3ac97aa53aa300617d
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-20 21:58:25 +0100

    imports

--- src/main/java/linkedin/web/DirtySecretsRestController.java
+++ src/main/java/linkedin/web/DirtySecretsRestController.java
@@ -2,8 +2,6 @@ package linkedin.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 public class DirtySecretsRestController {
 
