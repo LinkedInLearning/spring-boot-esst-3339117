038ad538be2ffc6f755a98b245e87b47db3d2a74
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-21 12:15:20 +0100

    clean

--- src/main/java/linkedin/data/GreetingRestController.java
+++ src/main/java/linkedin/data/GreetingRestController.java
@@ -1,7 +1,6 @@
 package linkedin.data;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
