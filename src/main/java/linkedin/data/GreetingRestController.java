240a32a633a8eaf9ad706db576e257af992b091a
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-21 12:17:01 +0100

    cleanup

--- src/main/java/linkedin/data/GreetingRestController.java
+++ src/main/java/linkedin/data/GreetingRestController.java
@@ -3,15 +3,14 @@ package linkedin.data;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingRestController {
 
   @GetMapping
-  public String get(@RequestParam(defaultValue = "America", required = false) String name) {
-    return String.format("%s, I'm only getting started.", name);
+  public String get() {
+    return "America, I'm only getting started.";
   }
 
 }
