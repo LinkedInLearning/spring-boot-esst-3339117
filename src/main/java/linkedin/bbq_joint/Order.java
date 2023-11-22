d1a86408af61851a48e8d006c95c5711a1ab0580
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-22 23:16:33 +0100

    cleanup

--- src/main/java/linkedin/bbq_joint/Order.java
+++ src/main/java/linkedin/bbq_joint/Order.java
@@ -1,17 +1,13 @@
 package linkedin.bbq_joint;
 
-import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity()
