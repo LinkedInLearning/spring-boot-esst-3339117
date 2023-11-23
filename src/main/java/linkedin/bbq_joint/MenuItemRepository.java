a5f9e32ac06c02abe8087cbd37288bf969b812b4
Jan Stamer <914776+remast@users.noreply.github.com>
2023-11-23 14:15:24 +0100

    Init

--- src/main/java/linkedin/bbq_joint/MenuItemRepository.java
+++ src/main/java/linkedin/bbq_joint/MenuItemRepository.java
@@ -1,28 +1,41 @@
 package linkedin.bbq_joint;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MenuItemRepository {
 
+  private Map<UUID, MenuItem> menuItems = new TreeMap<>();
+
   public Optional<MenuItem> findById(UUID id) {
-    return Optional.empty();
+    if (!menuItems.containsKey(id)) {
+      return Optional.empty();
+    }
+    return Optional.ofNullable(menuItems.get(id));
   }
 
   MenuItem save(MenuItem menuItem) {
+    if (menuItem.getId() == null) {
+      menuItem.setId(UUID.randomUUID());
+    }
+    menuItems.put(menuItem.getId(), menuItem);
     return menuItem;
   }
 
   public Iterable<MenuItem> findByOrderByDrinkDescNameDesc() {
-    return null;
+    return menuItems.values();
   }
 
   public Iterable<MenuItem> findByDrinkOrderByNameDesc(boolean drink) {
-    return null;
-
+    return menuItems.values().stream()
+        .filter((menuItem) -> menuItem.isDrink() == drink)
+        .collect(Collectors.toList());
   }
 
 }
